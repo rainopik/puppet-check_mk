7ae5d45876ed42cd3ab16af24dd44bbad40a09cb
Rain Öpik <rain.opik@gmail.com>
2014-09-23 16:08:55 +0300

    Improvements in OMD installation: omd and apache2 service name can be customized (the OMD package name may vary between repositories). Declaration of Apache can be disabled to avoid conflicts if it is already installed with another module like example42/apache.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,21 @@
+# Sets up and installs Open Monitoring Distribution with check_mk
+# 
+# Parameters:
+#   [*filestore*]         - directory for OMD package file. Set if you want to enable file-based installs, leave undef for installs from repository
+#   [*package*]           - package name (install from system repository) or package filename (file-based install)
+#   [*site*]              - OMD site name that will be created
+#   [*workspace*]         - temp dir for file-based installs
+#   [*omd_service_name*]  - name for the OMD service
+#   [*http_service_name*] - name for the Apache service. Leave undef for autodetection, set false if Apache service should not be handled (eg. defined in another module)
+#
 class check_mk (
-  $filestore   = undef,
-  $host_groups = undef,
-  $package     = 'omd-0.56',
-  $site        = 'monitoring',
-  $workspace   = '/root/check_mk',
+  $filestore            = undef,
+  $host_groups          = undef,
+  $package              = 'omd-0.56',
+  $site                 = 'monitoring',
+  $workspace            = '/root/check_mk',
+  $omd_service_name     = 'omd',
+  $http_service_name    = undef,
 ) {
   class { 'check_mk::install':
     filestore => $filestore,
@@ -17,6 +29,8 @@ class check_mk (
     require     => Class['check_mk::install'],
   }
   class { 'check_mk::service':
-    require   => Class['check_mk::config'],
+    http_service_name => $http_service_name,
+    omd_service_name  => $omd_service_name,
+    require           => Class['check_mk::config'],
   }
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,13 +1,33 @@
-class check_mk::service {
-  if ! defined(Service[httpd]) {
-    service { 'httpd':
-      ensure => 'running',
-      enable => true,
-    }
+class check_mk::service(
+  $http_service_name    = undef,
+  $omd_service_name     = 'omd',
+) {
+
+  $guess_http_service_name = $::osfamily ? {
+    /(?i-mx:redhat|suse|gentoo|linux)/ => 'httpd',
+    /(?i-mx:debian)/                   => 'apache2',
   }
+
+
+  $real_http_service_name = $http_service_name ? {
+    undef     => $guess_http_service_name,
+    default   => $http_service_name,    
+  }
+
+
+  if $http_service_name != false {
+    if ! defined(Service[$real_http_service_name]) {
+      service { $real_http_service_name:
+        ensure    => 'running',
+        enable    => true,
+      }
+    }    
+  }
+
+
   # xinetd service handled by xinetd module
-  service { 'omd':
-    ensure => 'running',
-    enable => true,
+  service { $omd_service_name:
+    ensure    => 'running',
+    enable    => true,
   }
 }
